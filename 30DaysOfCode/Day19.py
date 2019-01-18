2f001cb56fa253a16aa48e1503e78b881958f250
Silas Stulz <siou10@gmail.com>
2019-01-18 22:19:14 +0100

    day19

---
+++ 30DaysOfCode/Day19.py
@@ -0,0 +1,19 @@
+class AdvancedArithmetic(object):
+    def divisorSum(self, n):
+        raise NotImplementedError
+
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        divisors = []
+        for k in range(1, n + 1):
+            if n % k == 0:
+                divisors.append(k)
+        return sum(divisors)
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
